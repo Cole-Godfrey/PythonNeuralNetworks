00238f9b73787446d24d91d4b5004c505f3a0cd0
Cole Godfrey <coleg@Coles-MacBook-Pro.local>
2026-04-01 17:12:26 -0700

    Created a model

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 In this repository, I am building neural networks in Python using Tensorflow.  
 
-Currently I am loading and looking a data from the Fashion MNIST dataset.
+So far I have trained a small neural network on the Fashion MNIST dataset, achieving ~88% accuracy.
--- main.py
+++ main.py
@@ -13,7 +13,20 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-print(train_images[7])
+model = keras.Sequential([
+    keras.layers.Input(shape=(28, 28)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+    ])
 
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('\nTest accuracy:', test_acc)
+
